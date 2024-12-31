7dbdaba100730c775bde1d338df77767d177b1a4
Dimitri Hautot <3765206+DimitriHautot@users.noreply.github.com>
2024-12-31 12:48:51 +0100

    Après routage programmatique

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FaceSnap} from "../models/face-snap";
 import {FaceSnapComponent} from "../face-snap/face-snap.component";
 import {FaceSnapsService} from "../services/face-snaps.services";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-face-snap-list',
@@ -15,10 +16,14 @@ import {FaceSnapsService} from "../services/face-snaps.services";
 export class FaceSnapListComponent implements OnInit {
   faceSnaps!: FaceSnap[];
 
-  constructor(private faceSnapsService: FaceSnapsService) {
+  constructor(private faceSnapsService: FaceSnapsService, private router: Router) {
   }
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getFaceSnaps();
+  }
+
+  routeToLandingPage() {
+    this.router.navigateByUrl('');
   }
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
+  constructor(private router: Router) {
+  }
+
+  routeToFaceSnaps() {
+    this.router.navigateByUrl('/facesnaps');
+  }
 }
